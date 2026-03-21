f19f115cd3b2e100632815841fa757339711301d
ErnstSmith <88@gmail.com>
2026-03-21 19:19:06 +0300

    fix: упрощена интеграция Cloudinary через прямые URL

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,13 +1,24 @@
+from django.conf import settings
+
 from .models import TelegramUser
 
 
 def telegram_user(request):
     user_pk = request.session.get("telegram_user_pk")
     if not user_pk:
-        return {"telegram_user": None}
+        return {
+            "telegram_user": None,
+            "CLOUDINARY_CLOUD_NAME": settings.CLOUDINARY_CLOUD_NAME,
+        }
     try:
         user = TelegramUser.objects.get(pk=user_pk)
     except TelegramUser.DoesNotExist:
         request.session.pop("telegram_user_pk", None)
-        return {"telegram_user": None}
-    return {"telegram_user": user}
+        return {
+            "telegram_user": None,
+            "CLOUDINARY_CLOUD_NAME": settings.CLOUDINARY_CLOUD_NAME,
+        }
+    return {
+        "telegram_user": user,
+        "CLOUDINARY_CLOUD_NAME": settings.CLOUDINARY_CLOUD_NAME,
+    }
