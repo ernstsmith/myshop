f5cdbc33de9d0ff26d6afb413718ac987502ea06
ErnstSmith <88@gmail.com>
2026-03-22 19:32:34 +0300

    fix: убран total_price из создания заказа

--- shop/models.py
+++ shop/models.py
@@ -109,6 +109,7 @@ class Order(models.Model):
     tg_username = models.CharField(max_length=150, blank=True, default="")
 
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     paid = models.BooleanField(default=False)
 
